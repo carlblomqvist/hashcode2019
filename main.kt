7498b3b39fbe3eef76c9e1b120c1bcaab7820b27
Tias Persson <persma@student.chalmers.se>
2019-02-28 20:30:37 +0100

    toString

--- main.kt
+++ main.kt
@@ -1,10 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 
+
 object Hashcode {
     fun readFile(filename: String): List<String> = File(filename).readLines()
 
     var nopdone = 0
+
+    var picture1 = Picture(0, setOf("Söt", "Sweet"), 'H')
+    var picture2 = Picture(1, setOf("Sweet", "Swag"), 'V')
+    var slide1 = Slide(setOf(picture1, picture2))
+    var slideshow = Slideshow(2, listOf(slide1))
+
 
     @JvmStatic fun main(args:Array<String>) {
 
@@ -28,8 +35,18 @@ object Hashcode {
         println(line)
         return
     }
+
+    fun showSlideshow(){
+        val str: StringBuilder = StringBuilder()
+
+        str.append(slideshow.nrOfSlides)
+        for (slide in slideshow){
+            for ()
+        }
+    }
 }
 
 
 data class Picture(val id: Int, val tags: Set<String>, val orientation: Char)
-data class Slideshow(val nrOfSlides: Int, val slides: List<List<Picture>>)
+data class Slide(val pictures: Set<Picture>)
+data class Slideshow(val nrOfSlides: Int, val slides: List<Slide>)
